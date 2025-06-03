7a252917cd5058413ef448b7afb7c334cf9aa439
dev5-namaail <developer5@namaa-il.com>
2025-06-03 10:26:12 +0300

    create repo as atemplate by using react table lib

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import BasicTable from './components/BasicTable'
+
+function App() {
+
+  return (
+    <div style={{textAlign:'center'}}>
+        <BasicTable/>
+      
+    </div>
+      
+  
+  )
+}
+
+export default App
+
