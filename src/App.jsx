0bff7c3ed27116848a662157075210d8d796972e
dev5-namaail <160500974+dev5-namaail@users.noreply.github.com>
2025-06-04 12:06:45 +0300

    add GlobalFilteringTable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 import BasicTable from './components/BasicTable'
-
+import BasicTable from './components/BasicTable'
+import SortingTable from './components/SortingTable';
+import SortingTableFormatting from './components/SortingTableFormatting';
+import FilteringTable from './components/FilteringTable';
 function App() {
 
   return (
     <div style={{textAlign:'center'}}>
         <BasicTable/>
-      
+        <BasicTable/>
+        <SortingTable/>
+        <SortingTableFormatting/>
+        <FilteringTable/>
     </div>
       
   
