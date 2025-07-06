7d27f91920e97cf798a03905c561801740e2c086
dev5-namaail <marwa.fouad102@gmail.com>
2025-07-06 11:15:01 +0300

    updating to how components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import './App.css';
 import BasicTable from './components/BasicTable'
-import BasicTable from './components/BasicTable'
 import SortingTable from './components/SortingTable';
 import SortingTableFormatting from './components/SortingTableFormatting';
 import FilteringTable from './components/FilteringTable';
+import GlobalFilteringTablePagSelectedhidden from './components/GlobalFilteringTablePagSelectedhidden';
+import GlobalFilteringTablePagSelected from './components/GlobalFilteringTablePagSelected';
+import GlobalFilteringTablePag from './components/GlobalFilteringTablePag';
+import FilteringTablePagSelecteddoupleclick from './components/FilteringTablePagSelecteddoupleclick';
+
 function App() {
 
   return (
     <div style={{textAlign:'center'}}>
         <BasicTable/>
-        <BasicTable/>
         <SortingTable/>
         <SortingTableFormatting/>
         <FilteringTable/>
+        <GlobalFilteringTablePagSelectedhidden/>
+        <GlobalFilteringTablePagSelected/>
+        <GlobalFilteringTablePag/>
+        <FilteringTablePagSelecteddoupleclick/>
     </div>
       
   
